f8af020b77a74930112de133ccb497f361e2f438
chenyicai0819 <2495842975@qq.com>
2021-08-04 00:22:34 +0800

    首次提交springboot+mybatis+vue

---
+++ gytht/src/main/java/cn/edu/guet/GythtApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.guet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GythtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GythtApplication.class, args);
+	}
+
+}
